96d31cb010a308bab7effa243bc2a223b33d829a
Niko <123422869+Niko-Retrobricker@users.noreply.github.com>
2023-01-28 02:48:27 +0000

    anti-ban

--- testing/anti-place-ban.lua
+++ testing/anti-place-ban.lua
@@ -6,6 +6,8 @@ if game.PlaceId == 5846386835 then
     ReplicatedStorage.RemoteEvents.RequestPlayerTeleport:FireServer(args)
 end
 
+syn.queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/NikoBricker/NikoBricker/main/testing/anti-place-ban.lua'))()")
+
 
 -- menu: 5846386835
 -- player: 5867227007
